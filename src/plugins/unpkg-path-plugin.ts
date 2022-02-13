75826823bd63d4256b8083baff6627a7764a1d05
mfaizudd <mfaizudd@gmail.com>
2022-02-13 21:46:35 +0700

    refactor: fetch plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,12 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
-import localforage from 'localforage';
 
-const fileCache = localforage.createInstance({
-    name: "filecache",
-});
-
-export const unpkgPathPlugin = (inputCode: string) => {
+export const unpkgPathPlugin = () => {
     return {
         name: 'unpkg-path-plugin',
         setup(build: esbuild.PluginBuild) {
@@ -29,29 +23,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
                 const unpkgPath = `https://unpkg.com/${args.path}`;
                 return { path: unpkgPath, namespace: 'a' };
             })
-
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                console.log('onLoad', args);
-                if (args.path === 'index.js') {
-                    return {
-                        loader: 'jsx',
-                        contents: inputCode,
-                    };
-                }
-                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-                if (cachedResult) {
-                    return cachedResult;
-                }
-                const { data, request } = await axios.get(args.path);
-                const result: esbuild.OnLoadResult = {
-                    loader: 'jsx',
-                    contents: data,
-                    resolveDir: new URL('./', request.responseURL).pathname
-                };
-                await fileCache.setItem(args.path, result);
-
-                return result;
-            });
         },
     };
 };
