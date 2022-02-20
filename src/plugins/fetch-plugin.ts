278c84d9a89ebf6db23fb80fe6e065e7b142d2c2
mfaizudd <mfaizudd@gmail.com>
2022-02-20 17:01:08 +0700

    feat: css loader

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,6 +10,34 @@ export const fetchPlugin = (inputCode: string) => {
     return {
         name: 'fetch-plugin',
         setup(build: esbuild.PluginBuild) {
+            build.onLoad({ filter: /\.css$/ }, async (args: any) => {
+                console.log('onLoadCss', args);
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+                if (cachedResult) {
+                    return cachedResult;
+                }
+                const { data, request } = await axios.get(args.path);
+                const escaped = data
+                    .replace(/\n/g, '')
+                    .replace(/"/g, '\\"')
+                    .replace(/'/g, "\\'");
+                
+                const contents = 
+                    `
+                        const style = document.createElement('style');
+                        style.innerText = '${escaped}';
+                        document.head.appendChild(style);
+                    `;
+
+                const result: esbuild.OnLoadResult = {
+                    loader: 'jsx',
+                    contents,
+                    resolveDir: new URL('./', request.responseURL).pathname
+                };
+                await fileCache.setItem(args.path, result);
+
+                return result;
+            });
 
             build.onLoad({ filter: /.*/ }, async (args: any) => {
                 console.log('onLoad', args);
@@ -33,6 +61,7 @@ export const fetchPlugin = (inputCode: string) => {
 
                 return result;
             });
+
         }
     }
 }
